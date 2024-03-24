7b58840d6a4465bba304cfb88a232e51af0dffdb
yura-prots <yuraprots@gmail.com>
2024-03-24 18:48:35 +0200

    add starting files

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+// contacts.js
+
+/*
+ * Розкоментуй і запиши значення
+ * const contactsPath = ;
+ */
+
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
